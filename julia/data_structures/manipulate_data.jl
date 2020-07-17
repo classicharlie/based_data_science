f5f6097ca5acb56b76dd6f80223e9a812b9b1996
Charles Knipp <cknipp@asu.edu>
2020-07-17 10:10:46 -0700

    define cols as vars by eval() quoted strings

--- julia/data_structures/manipulate_data.jl
+++ julia/data_structures/manipulate_data.jl
@@ -2,3 +2,6 @@ include("./read_data.jl")
 
 # define the filepath and load the data set
 data = readCSV("mtcars.csv")
+
+# convert each column name into a variable
+for (k,v) in data @eval Meta.parse("k=v") end
--- python/data_structures/manipulate_data.py
+++ python/data_structures/manipulate_data.py
@@ -2,4 +2,6 @@ from read_data import readCSV
 
 # define the filepath to the csv and load the data set
 data = readCSV('mtcars.csv')
-print(data)
+
+# convert each column name into a variable
+for k,v in data.items(): exec(k+'='+str(v))
