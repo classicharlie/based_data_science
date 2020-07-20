39d32e9b1eaa7fc0f1d23d0c2fc99346333d6ab4
Charles Knipp <cknipp@asu.edu>
2020-07-20 08:42:00 -0700

    pretty printing

--- python/data_structures/manipulate_data.py
+++ python/data_structures/manipulate_data.py
@@ -1,7 +1,11 @@
 from read_data import readCSV
+import pprint
 
 # define the filepath to the csv and load the data set
 data = readCSV('mtcars.csv')
 
 # convert each column name into a variable
 for k,v in data.items(): exec(k+'='+str(v))
+
+# pretty print to make the dictionary look nicer in the console
+pprint.pprint(data,compact=True)
