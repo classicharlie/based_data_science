57570eb341b47c65e122bdfbb5905643a6c39e67
classicharlie <cknipp@asu.edu>
2020-07-23 13:37:57 -0700

    remove duplicate functions

--- julia/data_structures/monte_carlo.jl
+++ julia/data_structures/monte_carlo.jl
@@ -1,52 +1,19 @@
-"""
-monte carlo methods estimate the value of an unkown quantity using inferential
-statistics to draw conclusions from known data
-
-general process:
-    - define a domain of possible inputs
-    - generate inputs randomly from a probability distribution over the domain
-    - perform a deterministic computation on the inputs
-    - aggregate the results
-"""
-
 using Distributions
 using Plots
 
-function readCSV(filepath::String)
-	data = Dict{String,Array}()
+# include the csv reader to parse the data
+include("./read_data.jl")
 
-	# Not as efficient as python, but it does the job in less code
-	open(filepath) do file
-		header = map(x->replace(x," "=>"_"),split(readline(file),","))
-		[data[col]=[] for col in header]
-		rows = [split(line,",") for line in readlines(file)]
+"""
+	monteCarlo(data::Array,num_sims::Int,sim_obs::Int)
 
-		for i in 1:length(header)
-			[push!(data[header[i]],row[i]) for row in rows]
-		end
-	end
-
-	# this process is just fucking gross, but what else can I do
-	for (k,v) in data
-		try data[k] = map(x->parse(Int,x),v)
-		catch
-			try data[k] = map(x->parse(Float64,x),v)
-			catch
-				data[k] = map(x->String(x),v)
-			end
-		end
-	end
-
-	return data
-end
-
-# alias CartesianIndex because it takes up a lot of linespace
-idx(i,j) = CartesianIndex(i,j)
-
-
-# data is indexed starting at the most recent observation
+monte carlo methods estimate the value of an unkown quantity using inferential
+statistics to draw conclusions from known data; resulting matrix contains the
+sims which are lists indexed by the number of observations
+"""
 function monteCarlo(data::Array,num_sims::Int,sim_obs::Int)
-    num_obs = length(data)
+	idx(i,j) = CartesianIndex(i,j)
+    num_obs  = length(data)
 
     μ = sum(data)/num_obs
     σ = sqrt(sum((data.-μ).^2)/num_obs)
