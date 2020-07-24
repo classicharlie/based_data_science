d347c1dd235ab3e10328e9a898326bd5532c6247
Charles Knipp <cknipp@asu.edu>
2020-07-24 11:00:28 -0700

    implemented support for p-values

--- julia/least_squares/regression.jl
+++ julia/least_squares/regression.jl
@@ -10,42 +10,74 @@ end
 
 # custom method to print the regression table all nice and fancy
 function Base.show(io::IO,z::LeastSquares)
-    println(io,"\t\tβ\tse\tt")
+    println(io,"\t β\t se\t t")
     for k in keys(z.betas)
-        print(io,"$(@sprintf("%12s",k))","\t",
-            "$(@sprintf("%.2f",z.betas[k][1]))\t",
-            "$(@sprintf("%.3f",z.betas[k][2]))\t",
-            "$(@sprintf("%.3f",z.betas[k][3]))\n")
+        print(io,"$(@sprintf("%.5s",k))","\t",
+            "$(@sprintf("% .2f",z.betas[k][1]))\t",
+            "$(@sprintf("% .3f",z.betas[k][2]))\t",
+            "$(@sprintf("% .3f",z.betas[k][3]))  ",
+            z.betas[k][4],"\n")
     end
-    print(io,"\n\tR²:  $(@sprintf("%.4f",z.r_squared))")
+    println(io,"----\np-val:  0 ‘***’ .001 ‘**’ .01 ‘*’ .05 ‘.’ .1")
+    print(io,"\nR²:  $(@sprintf("%.4f",z.r_squared))")
 end
 
-function ls(formula::String,data::Dict)
-    # parse this bad bitch
-    parsed = parseFormula(formula,data)
-    labels = parsed[3]
+# convert t-value to p-value
+function pValue(t_value::Float64,df::Int64)
+    # find the corresponding df
+    include("./t_test.jl")
+    for k in df_keys
+        global idx
+        if k <= df
+            idx = k
+        else
+            break
+        end
+    end
+    
+    row = t_dist[idx]
+    num_cols = length(row)
+    
+    # find the nearest t-value and return α level
+    for i in 1:num_cols
+        global code
+        if row[i] <= abs(t_value)
+            code = conf[i]
+        else
+            break
+        end
+    end
 
-    A = parsed[1]
-    b = parsed[2]
+    return code
+end
+
+# least squares method for linear regression
+function ls(formula::String,data::Dict)
+    # A ≡ formula matrix, b ≡ dependent vector
+    A,b,labels = parseFormula(formula,data)
+
     n,m = size(A)
+    df = n-m
     
     # left division, or "\", is like inversion
-    betas  = (A'*A)\(A'*b)
-    errors = b-A*betas
+    β = (A'*A)\(A'*b)
+    ϵ = b-A*β
     b_hat  = sum(b)/n
 
     # find r-squared using sum of squares
-    sse = errors'*errors
+    sse = ϵ'*ϵ
     sst = b'*b-(n*b_hat^2)
     ssr = sst-sse
 
     # find the standard errors and eventually t-values
-    σ² = sse/(n-m)
+    σ² = sse/df
     invA = inv(A'*A)
-    se = [sqrt(σ²*invA[i,i]) for i in 1:size(betas,1)]
-    t  = [betas[i]/se[i] for i in 1:m]
+    se = [sqrt(σ²*invA[i,i]) for i in 1:size(β,1)]
 
-    betas_labelled = Dict(zip(labels,zip(betas,se,t)))
+    t = [β[i]/se[i] for i in 1:m]
+    p = [pValue(t_val,df) for t_val in t]
+
+    betas_labelled = Dict(zip(labels,zip(β,se,t,p)))
     model = LeastSquares(betas_labelled,1-(sse/sst))
 
     return model
